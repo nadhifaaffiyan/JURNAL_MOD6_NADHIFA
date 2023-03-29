7d7707068220438ae921f837130368cb803c432e
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-03-29 17:12:06 +0700

    penambahan desain by contract pada semua kelas

---
+++ SayaTubeUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+public class SayaTubeUser
+{
+	private int id;
+	private List<SayaTubeVideo>
+	public string title;
+
+	public SayaTubeUser(string judul)
+	{
+		Random.random = new Random();
+		this.id = random.Next(1, 99999);
+		Debug.Assert(judul != null);
+		Debug.Assert(judul.Length < 200);
+		this.title = judul
+
+		uploadedVideos = new List<SayaTubeVideo>();
+
+		if(judul == null
+		{
+			Console.Error.WriteLine("Tidak Boleh Kosong")
+		}
+		if(judul length > 200)
+		{
+			Console.Error.WriteLine("Tidak Boleh Kosong")
+		}
+	}
+
+	public int GetTotalVideoDetails()
+	{
+		return uploadVideo.Count;
+	}
+
+	public void AddVideo(SayaTubeVideo video)
+	{
+		Debug.Assert(video != null);
+		Debug.Assert(video.GetPlayCount);
+
+		uploadedVideos.Add(video);
+
+	}
+	public void PrintAllVideoPlayCount()
+	{
+		for (int i = 0; i < uploadedVideos.Count; i++)
+		{
+			Console.WriteLine("video:" + (i + 1) + uploadedVideos[i].GetPlayCount);
+		}
+	}
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+        public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+    }
+    public SayaTubeVideo(string a)
+    {
+        Random random = new Random();
+        this.id = random.Next(1, 99999)
+
+        this.playcount;
+ = 0;
+    }
+    void increasePlayCount(int playcount)
+    {
+        this.playcount = playcount;
+        playcount = playcount + 1;
+
+    }
+
+    void GetPlayCount()
+    {
+        return playcount;
+    }
+    void PrintVideodetails()
+    {
+        Console.WriteLine("ID" + this.id);
+        Console.WriteLine("Title" + this.Title);
+        Console.WriteLine("Play Count" + playcount);
+
+    }
+}
